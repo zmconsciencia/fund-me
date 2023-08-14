b679108e3f843076d9fafcea817ff394928104dd
zmconsciencia <jconsciencia1999@gmail.com>
2023-08-14 17:30:49 +0100

    test to check owner

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -15,4 +15,8 @@ contract FundMeTest is Test {
     function testMinDollar() public {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
+
+    function testOwner() public {
+        assertEq(fundMe.i_owner(), address(this));
+    }
 }
